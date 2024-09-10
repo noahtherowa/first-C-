96f9b6ce02f8bc94fd6720d7c402ebe31992c554
noahtherowa <159577690+noahtherowa@users.noreply.github.com>
2024-09-10 09:14:28 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age = 5;
+float b = 6.5;
+char c = 'c';
+bool d = true;
+string e = "Hello world";
+
+    if (d){
+    cout << age << endl;
+   cout << b << endl;
+    cout << c << endl;
+     cout << e << endl;
+    }
+   
+}
